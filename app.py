12389cd1285090adf0aade31ada5db101174ea8d
JoaoVicttorsMelo <joaovicttorsilveiramelo@gmail.com>
2022-06-27 18:02:28 -0300

    Criando os primeiros passos do projeto

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+from resources.hotel import Hoteis, Hotel
+
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#http://127.0.0.1:5000/hoteis
---
+++ resources/hotel.py
@@ -0,0 +1,70 @@
+from flask_restful import Resource, Api, reqparse
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 320.20,
+        'cidade': 'Santa Catarina'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {
+        'Hoteis': hoteis
+        }
+
+
+class Hotel(Resource):
+
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404 #not found
+
+
+    def post(self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+
+    def put(self, hotel_id):
+        pass
+
+
+    def delete(self, hotel_id):
+        pass
